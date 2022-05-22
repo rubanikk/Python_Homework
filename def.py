5371ef6eca62b2e553a24a2cca1b5e5ffa61385c
rubanikk <rubanik.k@gmail.com>
2022-05-22 21:49:09 +0200

    first commit

---
+++ def.py
@@ -0,0 +1,10 @@
+a = int(input('введите значение а: '))
+b = int(input('введите значение b: '))
+c = int(input('введите значение c: '))
+def cycle (a,b,c):
+    while a < b:
+        print('sad message')
+        a = a + c
+    else:
+        print('happy message')
+cycle (a,b,c)
---
+++ variables.py
@@ -0,0 +1,4 @@
+a = input('enter your first name: ')
+b = input('enter your last name: ')
+c = input('enter your age: ')
+print(a + ' ' + b + ',' + ' ' + c + '.')
