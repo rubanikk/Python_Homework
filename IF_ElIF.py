dd9d88610806753ccf1961bd079182ea9a1f2ada
rubanikk <rubanik.k@gmail.com>
2022-05-23 13:02:47 +0200

    new version if_elif

---
+++ IF_ElIF.py
@@ -0,0 +1,11 @@
+a = int(input('введите чило а:  '))
+b = int(input('введите чило b:  '))
+c = int(input('введите чило c:  '))
+if a>b:
+	print('sad message')
+elif a==b:
+	print('equality')
+elif b>c:
+	print('happy message')
+else:
+	print('not a very sad message')
