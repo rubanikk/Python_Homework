e04b04bb06d2b70bee8b344dff04bd6e7220ab68
rubanikk <rubanik.k@gmail.com>
2022-05-23 13:51:42 +0200

    new version

--- def.py
+++ def.py
@@ -1,10 +1,15 @@
-a = int(input('введите значение а: '))
-b = int(input('введите значение b: '))
-c = int(input('введите значение c: '))
 def cycle (a,b,c):
+    d = str(a)
     while a < b:
         print('sad message')
         a = a + c
+        d = d + ' ' + str(a)
     else:
         print('happy message')
-cycle (a,b,c)
+        print('история инкрементов: ', d)
+
+a = int(input('введите значение а: '))
+b = int(input('введите значение b: '))
+c = int(input('введите значение c: '))
+
+cycle(a,b,c)
