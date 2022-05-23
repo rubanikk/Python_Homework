aafad959bbea4608644d543015f45400e7eeb17a
rubanikk <rubanik.k@gmail.com>
2022-05-23 12:42:54 +0200

    new version

--- while.py
+++ while.py
@@ -1,1 +1,12 @@
-print('dfjkghdfjkh')
+a = int(input('введите значение а: '))
+b = int(input('введите значение b: '))
+c = int(input('введите значение c: '))
+d = str(a)
+
+while (a < b):
+    print('sad message')
+    a = a + c
+    d = d + ' ' + str(a)
+
+print('happy message')
+print('история инкрементов: ', d)
